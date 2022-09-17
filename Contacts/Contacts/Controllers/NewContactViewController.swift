e2f15275954b390cf405047ed8a6345c8d64bae9
Nestertsov Maksym <nestertsovmaxim@gmail.com>
2022-09-17 15:46:50 +0300

    Created UI

---
+++ Contacts/Contacts/Controllers/NewContactViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewContactViewController.swift
+//  Contacts
+//
+//  Created by M1 on 17.09.2022.
+//
+
+import UIKit
+
+class NewContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
